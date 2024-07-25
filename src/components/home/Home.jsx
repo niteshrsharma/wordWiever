bc0761d690840c4507bf64f215cf514cdcded400
niteshrsharma <nitesh.r.sharma999@gmail.com>
2024-07-25 14:04:27 +0530

    second commit

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,7 +1,7 @@
 export default function Home(){
     return (
         <>
-            <h1>hello this is home</h1>
+            <h1>hello this is home and this is changed file</h1>
         </>
     )
 }
